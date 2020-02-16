a30158504a286e402e311513f6e2e44ac58bd513
Mateusz Pietrzak <simcitymrsuperslid@gmail.com>
2020-02-16 23:10:53 +0100

    Add comments

--- books/admin.py
+++ books/admin.py
@@ -1,14 +1,17 @@
 from django.contrib import admin
-from .models import Book, Author, Category
+from .models import Book, Author, Category, Comment
 # Register your models here.
 from django.db import models
 
 class BookAdmin(admin.ModelAdmin):
-    readonly_fields = ('book_likes',)
+    readonly_fields = ('book_likes', 'pub_date')
     fieldsets = [
         ('Ogólne informacje', {'fields':['book_title', 'book_author', 'book_desc', 'book_category', 'book_img', 'pub_date']}),
+        ('Informacje od użytkowników', {'fields': ['book_likes', 'comments', ]}),
     ]
+
 
 admin.site.register(Book, BookAdmin)
 admin.site.register(Author)
 admin.site.register(Category)
+admin.site.register(Comment)
--- books/models.py
+++ books/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Author(models.Model):
@@ -11,16 +12,27 @@ class Category(models.Model):
     def __str__(self):
         return self.category_name
 
+
+class Comment(models.Model):
+    content = models.TextField("Treść komentarza", max_length=500)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="Użytkownik")
+    pub_date = models.DateTimeField('Data utworzenia', auto_now_add=True)
+
+    def __str__(self):
+        return self.content[:10]
+
+
 class Book(models.Model):
     book_title = models.CharField("Tytuł książki", max_length=50)
     book_desc = models.TextField("Opis książki", max_length=None)
     book_img = models.ImageField("Okładka książki", blank=True, null=True, upload_to="covers/%Y/%m/%D/")
     book_author = models.ManyToManyField(Author, verbose_name="Autor/Autorzy")
     book_category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name="Kategoria")
-    pub_date = models.DateTimeField('date published')
+    pub_date = models.DateTimeField('Data utworzenia', auto_now_add=True)
     book_likes = models.IntegerField("Ocena", default=0)
+    comments = models.ManyToManyField(Comment, verbose_name="Komentarze")
+
     def __str__(self):
         return self.book_title
-
 
 
